2e63dd546aa8393d386c55393838de171bb4c4ba
Roberto Chagas <roberto.ca.dev@gmail.com>
2023-10-26 22:58:16 -0300

    Deck Name Fix

--- resources/views/create_deck.blade.php
+++ resources/views/create_deck.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>criar deck</title>
+    <title>Criar Baralho</title>
     @vite('resources/css/app.css')
 </head>
 <body>
@@ -11,7 +11,7 @@
     <div  class="flex flex-col justify-center items-center  h-96 w-64 p-4 shadow-2xl space-y-10 bg-neutral-50  ">
    <form action="{{route('gerardeck')}}" method="post" class="form">
     @csrf
-   <label for="title">De um nome ao deck</label>
+   <label for="title">De um nome ao baralho</label>
    <input type="text" name="title" class="border-b-2 border-indigo-600 my-3"></input>
    <label for="description">De uma descrição</label>
    <input type="text" name="description" class="border-b-2 border-indigo-600 my-3"></input>
